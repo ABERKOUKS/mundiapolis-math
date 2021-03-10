9bb37f710dff234ded22155d3e0ee70d72bd5345
ABERKOUKS Hafssa <66857045+ABERKOUKS@users.noreply.github.com>
2021-03-10 22:45:03 +0100

    Update 13-cats_got_your_tongue.py

--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -1,8 +1,4 @@
 import numpy as np
 def np_cat(mat1, mat2, axis=0):
-    if len(mat1[0])==len(mat2[0]) and axis==0:
-        conc=np.concatenate((mat1, mat2),axis=0) 
-        return conc.tolist()       
-    elif len(mat1)==len(mat2) and axis==1:
-        conc=np.concatenate((mat1, mat2),axis=1) 
+        conc=np.concatenate((mat1, mat2),axis=axis) 
         return conc.tolist()  
