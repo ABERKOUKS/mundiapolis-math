3a1358cb79e6d4e6ab19b805dd45f49b02db62a3
ABERKOUKS Hafssa <66857045+ABERKOUKS@users.noreply.github.com>
2021-03-10 23:42:14 +0100

    Add files via upload

---
+++ math/0x00-linear_algebra/102-squashed_like_sardines.py
@@ -0,0 +1,10 @@
+import numpy as np
+def cat_matrices(mat1, mat2, axis=0):
+    try:
+        return np.concatenate((mat1, mat2), axis).tolist()
+        return np.concatenate(
+            (mat1, mat2), axis, out=None
+        ).tolist()
+    except ValueError:
+        return None
+		
