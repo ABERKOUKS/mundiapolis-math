e88590a05207feabb2f9112470d90ed56fbf1888
ABERKOUKS Hafssa <66857045+ABERKOUKS@users.noreply.github.com>
2021-03-10 14:23:42 +0100

    Add files via upload

---
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,9 @@
+import numpy 
+def mat_mul(mat1, mat2):
+    if len(mat1[0])== len(mat2):
+        mat1= numpy.asarray(mat1)
+        mat2= numpy.asarray(mat2)
+        mult=mat1.dot(mat2) 
+        return mult.tolist()    
+    else:
+        return "None"
