68c1238b4df6ece223a0ebb416366fbc50a59549
ABERKOUKS Hafssa <66857045+ABERKOUKS@users.noreply.github.com>
2021-03-12 17:06:22 +0100

    Update 5-across_the_planes.py

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3 
 def add_matrices2D(mat1, mat2):
-    mat1=numpy.array(mat1)
-    mat2=numpy.array(mat2)
     if len(mat1) == len(mat2) and len(mat1[0]) == len(mat2[0]) :
         addM=[]
-        addM=[[mat1[i][j]+mat2[i][j] i in range(len(mat1))] for j in range(len(mat1[0]))]
+        addM=[[mat1[i][j]+mat2[i][j] for j in range(len(mat1[0]))] i in range(len(mat1))]
         return addM
     else:
         return "None"
