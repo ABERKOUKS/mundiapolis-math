a971fe43650ac6fb0be5313cbfc7a2a53ff95d7f
ABERKOUKS Hafssa <66857045+ABERKOUKS@users.noreply.github.com>
2021-03-12 15:26:39 +0100

    Update 2-size_me_please.py

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 def matrix_shape(matrix):
   if type(matrix[0]) is not list:
-    return [len(matrix)]
+    return int([len(matrix)])
   else:
-    return [len(matrix)]+matrix_shape(matrixe[0])
+    return int([len(matrix)]+matrix_shape(matrixe[0]))
