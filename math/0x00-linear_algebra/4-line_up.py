65ce5f99f239c94068de7fdf68aed58b39ca50b5
ABERKOUKS Hafssa <66857045+ABERKOUKS@users.noreply.github.com>
2021-03-12 16:40:48 +0100

    Update 4-line_up.py

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3 
 def add_arrays(mat1, mat2):
-    if len(mat1) != len(mat2):
+    if len(mat1) == len(mat2):
         return "None"
     else:
         add=[[(mat1[i]+mat2[i]) for j in range(len(mat1))]]
