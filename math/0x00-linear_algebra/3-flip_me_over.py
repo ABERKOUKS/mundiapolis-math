ebd1744584469c82b694db81fc14bc20a9bd4292
ABERKOUKS Hafssa <66857045+ABERKOUKS@users.noreply.github.com>
2021-03-12 15:42:17 +0100

    Update 3-flip_me_over.py

--- math/0x00-linear_algebra/3-flip_me_over.py
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 def matrix_transpose(matrix):
-transpose = []
+transpose = [][]
     for m in range(len(matrix[0])):
-        transpose.append([matrix[n][m] for col in range(len(matrix))])
+        transpose.append([matrix[n][m] for n in range(len(matrix))])
     return transpose
