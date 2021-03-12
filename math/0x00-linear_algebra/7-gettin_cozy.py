e40dd81f2bf85d278082859825e5336bc2c00c9c
ABERKOUKS Hafssa <66857045+ABERKOUKS@users.noreply.github.com>
2021-03-12 17:22:17 +0100

    Update 7-gettin_cozy.py

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -1,10 +1,10 @@
-import numpy 
+#!/usr/bin/env python3
 def cat_matrices2D(mat1, mat2, axis=0):
     if len(mat1[0])==len(mat2[0]) and axis==0:
-        conc=numpy.concatenate((mat1, mat2),axis=0) 
-        return conc.tolist()       
+        conc=vstack((mat1, mat2),axis=0) 
+        return conc    
     elif len(mat1)==len(mat2) and axis==1:
-        conc=numpy.concatenate((mat1, mat2),axis=1) 
-        return conc.tolist()     
+        conc=hstack(mat1, mat2),axis=1) 
+        return conc   
     else:
         return "None"
